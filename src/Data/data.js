dc215eb4490921e1a3137bb731641b4024adad94
Kathryn Lam <katwingki@gmail.com>
2020-12-30 01:55:57 -0500

    FS:Changes to the Home.js, looks good will stay with the white bkg. SP: Work on Projects.js, temp taken out AboutMe.js

--- src/Data/data.js
+++ src/Data/data.js
@@ -1,8 +1,41 @@
 export const aboutMe = {
   motto:
     'All our dreams can come true, if we have the courage to pursue them - by Walt Disney',
-  about:
-    'After my amazing 10+ years journey as an entrepreneur who sell, design, market, and research herbal supplements, I am more than grateful to have my dream came true, becoming a Software Engineer. Thanks to the Fullstack Academy Grace Hopper program where I acquire a tremendous amount of knowledge as well as full of hands-on experiences such as develop an application with a team via Agile Project Management, transforming my educational ideas into action by building different mobile applications, and much more. The teaching fellowship further deepen my knowledge and fulfill my passion as a mentor where I can help others understand new concepts and achieve their goals.',
+  aboutOne:
+    'After my amazing 10+ years journey as an entrepreneur who sell, design, market, and research herbal supplements, I am more than grateful to have my dream came true, becoming a Software Engineer. ',
+  aboutTwo:
+    'Thanks to the Fullstack Academy Grace Hopper program where I acquire a tremendous amount of knowledge as well as full of hands-on experiences such as develop an application with a team via Agile Project Management, transforming my educational ideas into action by building different mobile applications, and much more. ',
+  aboutThree:
+    'The teaching fellowship at Fullstack Academy further deepen my knowledge and fulfill my passion as a mentor where I can help others understand new concepts and achieve their goals.',
   keyValue:
     'I am ready and looking forward to any new challenges that I will be facing as a Software Engineer. I am particularly interested in working as a team as I believe teamwork is the key to success. My goal is to develop products that not only satisfy but exceed client’s expectations. ',
 };
+
+export const work = [
+  {
+    company: 'Fullstack Academy',
+    title: 'Teaching Fellow',
+    dates: 'NOV 2020 - FEB 2021',
+    descriptions: [
+      'Mentor, support and teach over 50 students in an immersive full-stack engineering program',
+      'Guide students on JavaScript fundamentals, debugging strategies, and software architecture',
+      ' Supervise student project teams by providing technical guidance and leading daily code review with agile methodologies ',
+      'Administer technical interviews for prospective students applying to the program',
+    ],
+  },
+];
+
+export const projects = [
+  {
+    title: 'BrainTeez',
+    github: 'https://github.com/team-g-capstone',
+    link: 'n/a',
+    youtube: 'https://www.youtube.com/watch?v=J4UF711Ofc0',
+    Date: 'October 2020',
+    descriptions: [
+      'BrainTeez is an educational mobile application built within 2.5 weeks by a team of three developers, using Firebase, Firestore, React Native, Expo, JavaScript, and Redux. Our goal was to create a holistic learning application that provides a variety of leveled games and a real-time multiplayer game room that engage many parts of the child’s brain at once.',
+      'Currently, there are three different leveled games. The first is a simple addition game that rendered with eye-catching pizazz. The second game strengthens logical skills and pattern-matching abilities by sorting a series of draggable circles into boxes by their color. The third game relies on the players’ memory to match pairs of cards with the same animal on them. All three games are progressively harder at each level, we believe the balance of shapes, numbers, colors, and sounds in these games will simulate new connections in the child’s brain. Finally, the real-time multiplayer game room allows the teacher to administer games as a test to evaluate the students’ performance. ',
+      'I am solely responsible for Firebase Authentication, and real-time multiplayer game room using Cloud Firestore.',
+    ],
+  },
+];
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,16 +4,18 @@ import AboutMe from './AboutMe';
 import Projects from './Projects';
 import { aboutMe } from '../Data/data';
 import Lottie from 'react-lottie';
-import animatedBigBlueCircles from '../lotties/bigBlueCircles.json';
+import linesPurpleBlue from '../lotties/lines_purple_blue.json';
+import lines from '../lotties/lines.json';
 import { motion } from 'framer-motion';
 
 function Home() {
   const defaultOptions = {
     loop: true,
     autoplay: true,
-    animationData: animatedBigBlueCircles,
+    animationData: linesPurpleBlue,
     rendererSettings: {
       preserveAspectRaiot: 'xMidYMid slice',
+      className: 'lottie-animation',
     },
   };
 
@@ -27,13 +29,36 @@ function Home() {
           >
             Hi there! I'm Kathryn, a full-stack developer.
           </motion.h2>
+
           <div className='intro-text'>
-            <p>{aboutMe.about}</p>
+            <p>{aboutMe.aboutOne}</p>
           </div>
-          <Lottie options={defaultOptions} height={150} width={150} />
+          <div className='intro-text'>
+            <p>{aboutMe.aboutTwo}</p>
+          </div>
+          <div className='intro-text'>
+            <p>{aboutMe.aboutThree}</p>
+          </div>
+          <div className='intro-text'>
+            <p>{aboutMe.keyValue}</p>
+          </div>
+
+          <div className='lottie-wrapper'>
+            <Lottie options={defaultOptions} />
+          </div>
+          <div className='center-wrapper'>
+            <motion.h3
+              animate={{ scale: [1, 6, 2, 1, 1], rotate: 360 }}
+              transition={{ delay: 0.6, duration: 0.6 }}
+              className='center-subtitle'
+            >
+              When I am not coding...
+            </motion.h3>
+          </div>
         </div>
       </div>
-      <AboutMe {...aboutMe} />
+
+      {/* <AboutMe {...aboutMe} /> */}
       <Projects />
     </>
   );
