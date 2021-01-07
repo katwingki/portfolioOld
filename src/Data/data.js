36888b205bd3667bbd62aec85b098ab02b074330
Kathryn Lam <katwingki@gmail.com>
2021-01-07 12:41:45 -0500

    FS: added all projects. SP: need to add all links to data.js. Then work on the links, resume , and comment form

--- src/Data/data.js
+++ src/Data/data.js
@@ -28,6 +28,8 @@ export const work = [
 export const projects = [
   {
     title: 'BrainTeez',
+    iframelink:
+      'https://player.cloudinary.com/embed/?cloud_name=demo&public_id=elephants&fluid=true&controls=true&source_types%5B0%5D=mp4',
     github: 'https://github.com/team-g-capstone',
     link: 'https://expo.io/@katwingki/projects/brainteez',
     youtube: 'https://www.youtube.com/watch?v=J4UF711Ofc0',
@@ -36,6 +38,34 @@ export const projects = [
       'BrainTeez is an educational mobile application built within 2.5 weeks by a team of three developers, using Firebase, Firestore, React Native, Expo, JavaScript, and Redux. Our goal was to create a holistic learning application that provides a variety of leveled games and a real-time multiplayer game room that engage many parts of the child’s brain at once.',
       'Currently, there are three different leveled games. The first is a simple addition game that rendered with eye-catching pizazz. The second game strengthens logical skills and pattern-matching abilities by sorting a series of draggable circles into boxes by their color. The third game relies on the players’ memory to match pairs of cards with the same animal on them. All three games are progressively harder at each level, we believe the balance of shapes, numbers, colors, and sounds in these games will simulate new connections in the child’s brain. Finally, the real-time multiplayer game room allows the teacher to administer games as a test to evaluate the students’ performance. ',
       'I am solely responsible for Firebase Authentication, and real-time multiplayer game room using Cloud Firestore.',
+    ],
+  },
+  {
+    title: 'Minions AI',
+    iframelink:
+      'https://res.cloudinary.com/dp8rfxspl/video/upload/ac_none,br_2303kk,ki_1,q_auto:best,vc_auto/ac_none,e_accelerate:100,ki_4/v1609658529/Portfolio/MinionsAI_CompletedWithAudio_pn4qcn.mp4',
+    github: 'https://github.com/katwingki/Stackathon',
+    link:
+      'https://res.cloudinary.com/dp8rfxspl/video/upload/v1609658529/Portfolio/MinionsAI_CompletedWithAudio_pn4qcn.mp4',
+    youtube:
+      'https://youtu.be/ngUMmZCazzc?list=PLUNbWIDGJfl5Le7wT5C9D9pJbk2s1H2gi',
+    date: 'October 2020',
+    descriptions: [
+      'Minions AI, an IOS, Android mobile application, that I created during a 4-day solo project, using React Native, Expo, JavaScript, and React. It is a simple and fun artificial intelligence mobile application that tests kids’ basic knowledge. ',
+      'Testing is an important part of learning, however, traditional paper and pen testing can be boring and stressful for kids, and possibly not the best indicator of the students’ potential. In this application, I incorporated some fun Minions elements where kids can ask Minions questions and received a response, vice versa, Minions can ask kids questions to see if they are smart enough to be their next master. The testing component is integrated with neural network-based Brain.js library and the training data was normalized with natural npm package.',
+      'After accumulating a strong foundation in building PERN Stack with agile methodologies, I decided to challenge myself with some new technologies such as React Native, and Expo for this solo project. One of the big challenges was the natural.js library is not compatible with Expo, I work around it by standardizing the data in React. Overall, I enjoyed the process of trial and error, and I was happy about what I was able to build within the time frame given. ',
+    ],
+  },
+  {
+    title: 'HYKM Bookstore',
+    github: 'https://github.com/HYKM/graceshopper',
+    link: 'https://hykm-bookstore.herokuapp.com',
+    youtube: 'https://www.youtube.com/watch?v=Xxc0v6FW38k',
+    date: 'September 2020',
+    descriptions: [
+      'HYKM Bookstore is a mock fully functional e-commerce website that built within 7-day by a team of four developers. The site sells textbooks with enhanced admin and security features. The application was developed with Node.js, PostgresSQL, Sequelize, Express, React, Redux, HTML, CSS, and OAuth. It was deployed to Heroku via Travis CI for continuous integration.',
+      'We built the application from the ground up using all the technologies that we acquired throughout the course. This was our first group project as a developer, we followed the Agile Software Development methodology to ensure all the requirements are met through the collaboration of the team, for example, we meet daily as a group to update each other on progress and divide the daily task among the group. For the more challenging part, we often pair-program to maximize efficiency. In addition, a teaching fellow was assigned as our project manager to guide us through the process and perform a quality check to ensure we are delivering qualified slices.',
+      'I am solely responsible for Heroku deployment and setting up authentication with Google using OAuth. ',
     ],
   },
 ];
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,6 +5,7 @@ import { projects } from '../Data/data';
 import { AiFillGithub, AiOutlineLink } from 'react-icons/ai';
 import { TiSocialYoutubeCircular } from 'react-icons/ti';
 const Projects = () => {
+  console.log('projects', projects);
   return (
     <div className='project-section'>
       <h1>Projects</h1>
@@ -12,48 +13,45 @@ const Projects = () => {
         Here is a list of projects built during the last 4 weeks of the program
         at Fullstack Academy Grace Hopper
       </h4>
-      <div id='main-container-projects'>
-        <div className='project-row'>
-          <div className='project-column'>
-            <div className='project-text-wrapper'>
-              <h2>{projects[0].title}</h2>
-              <h5>Fullstack Developer | {projects[0].date} </h5>
-              <p>{projects[0].descriptions[0]}</p>
-              <p>{projects[0].descriptions[1]}</p>
-              <p>{projects[0].descriptions[2]}</p>
-            </div>
-          </div>
-          <div className='project-column'>
-            <div className='project-right-wrapper'>
-              <iframe
-                src='https://player.cloudinary.com/embed/?cloud_name=demo&public_id=elephants&fluid=true&controls=true&source_types%5B0%5D=mp4'
-                id='projectIframe'
-                allow='autoplay; fullscreen; encrypted-media; picture-in-picture'
-                allowfullscreen
-                frameborder='0'
-              ></iframe>
-            </div>
-          </div>
 
-          <div className='project-social-icon-wrap '>
-            <a className='project-social-icon-link' href={projects[0].link}>
-              <AiOutlineLink />
-            </a>
-            <a
-              className='project-social-icon-link'
-              href='https://github.com/katwingki'
-            >
-              <AiFillGithub />
-            </a>
-            <a
-              className='project-social-icon-link'
-              href='https://github.com/katwingki'
-            >
-              <TiSocialYoutubeCircular />
-            </a>
+      {projects.map((project, i) => (
+        <div id='main-container-projects'>
+          <div className='project-row'>
+            <div className='project-column'>
+              <div className='project-text-wrapper'>
+                <h2>{project.title}</h2>
+                <h5>Fullstack Developer | {project.date} </h5>
+                <p>{project.descriptions[0]}</p>
+                <p>{project.descriptions[1]}</p>
+                <p>{project.descriptions[2]}</p>
+              </div>
+            </div>
+            <div className='project-column'>
+              <div className='project-right-wrapper'>
+                <iframe
+                  src={project.iframelink}
+                  id='projectIframe'
+                  allow='autoplay; fullscreen; encrypted-media; picture-in-picture'
+                  allowfullscreen
+                  frameborder='0'
+                ></iframe>
+              </div>
+            </div>
+
+            <div className='project-social-icon-wrap '>
+              <a className='project-social-icon-link' href={project.link}>
+                <AiOutlineLink />
+              </a>
+              <a className='project-social-icon-link' href={project.github}>
+                <AiFillGithub />
+              </a>
+              <a className='project-social-icon-link' href={project.youtube}>
+                <TiSocialYoutubeCircular />
+              </a>
+            </div>
           </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
